e4165884f1ff16b6beb5cb6b524c8078de335709
Глебов Эдуард <eduard_glebov@inbox.ru>
2015-05-24 00:55:41 +0500

    lab10

---
+++ WebApi/Controllers/api/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Ajax.Utilities;
+using WebApi.Models;
+
+namespace WebApi.Controllers.api
+{
+    public class OrdersController : ApiController
+    {
+        private static readonly Repository Repository = new Repository(HttpContext.Current.Server.MapPath("~/App_Data/orders.csv"));
+
+        [HttpGet]
+        public IEnumerable<Orders> GetAll()
+        {
+            return Repository.OrderList;
+        }
+
+        [HttpPost]
+        public void Delete(int id)
+        {
+            if (!Repository.DeleteOrder(id))
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+
+        [HttpPost]
+        public Orders Add(Orders order)
+        {
+            return Repository.AddOrder(order);
+        }
+
+    }
+}
---
+++ WebApi/Models/Repository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using WebApi.Controllers.api;
+
+namespace WebApi.Models
+{
+    public class Repository
+    {
+        private ObservableCollection<Orders> orderList = new ObservableCollection<Orders>();
+        private int id = 1;
+        private readonly string filePath;
+
+        public ObservableCollection<Orders> OrderList
+        {
+            get { return orderList; }
+        }
+
+        public Repository(string filePath1)
+        {
+            filePath = filePath1;
+            LoadData();
+            OrderList.CollectionChanged += (sender, args) =>
+            {
+                var output = JsonConvert.SerializeObject(orderList);
+                using (var json = new StreamWriter(filePath, false))
+                {
+                    json.Write(output);
+                }
+            };
+        }
+
+        public void LoadData()
+        {
+            using (var r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                orderList = JsonConvert.DeserializeObject<ObservableCollection<Orders>>(json);
+            }
+        }
+
+
+        public Orders AddOrder(Orders order)
+        {
+            order.Id = id++;
+            orderList.Add(order);
+            return order;
+        }
+
+        public bool DeleteOrder(int id)
+        {
+            var orderToDelete = orderList.First(order => order.Id == id);
+            return orderToDelete != null && orderList.Remove(orderToDelete);
+        }
+
+
+    }
+}
